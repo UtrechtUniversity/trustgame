9a75fe9c75796ca1c77cf820fc0d44b10fbdd586
robfranken <R.franken95@gmail.com>
2025-10-28 14:38:58 +0100

    update
    asynchronous investment game

--- settings.py
+++ settings.py
@@ -3,9 +3,16 @@ from os import environ
 SESSION_CONFIGS = [
     dict(
         name='Trust_Game',
-        num_demo_participants=10,
-        app_sequence=['Intro', 'PlayerA', 'PlayerB']
+        num_demo_participants=100,
+        app_sequence=['role_assignment', 'truster', 'trustee']
     )
+]
+
+ROOMS = [
+    dict(
+        name='ODISSEI',
+        display_name='ODISSEI'
+    ),
 ]
 
 SESSION_CONFIG_DEFAULTS = dict(
@@ -18,7 +25,7 @@ USE_POINTS = True
 DEMO_PAGE_INTRO_HTML = ''
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
-ROOMS = []
+
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
